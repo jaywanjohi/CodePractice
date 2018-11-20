7082be34c25a37045ea96c22190b5116b849e51b
JayJay <jwanjohi95@gmail.com>
2018-11-20 23:45:25 +0000

    Check permutation of two strings

---
+++ src/ArraysAndStrings/CheckPermutation.java
@@ -0,0 +1,15 @@
+package ArraysAndStrings;
+
+public class CheckPermutation {
+    String sort (String s) {
+        char[] content = s.toCharArray();
+        java.util.Arrays.sort(content);
+        return new String(content);
+    }
+    boolean permutation(String s, String t){
+        if(s.length() != t.length()){
+            return false;
+        }
+        return sort(s).equals(sort(t));
+    }
+}
--- src/ArraysAndStrings/IsUnique.java
+++ src/ArraysAndStrings/IsUnique.java
@@ -13,4 +13,3 @@ public class IsUnique {
         return true;
     }
 }
-
