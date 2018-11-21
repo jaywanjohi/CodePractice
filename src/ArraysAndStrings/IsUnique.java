752cb7f25ef01a3358ef884816097903d4801d2f
JayJay <jwanjohi95@gmail.com>
2018-11-21 18:21:53 +0000

    Add comments and test code

--- src/ArraysAndStrings/IsUnique.java
+++ src/ArraysAndStrings/IsUnique.java
@@ -1,15 +1,27 @@
 package ArraysAndStrings;
 
 public class IsUnique {
-    boolean isUniqueChars(String str){
+    public static void main(String[] args) {
+        String test = "Anthony";
+        System.out.println("Uniqueness test: " + isUniqueChars(test));
+    }
+    private static boolean isUniqueChars(String str){
+        // Assuming string can have characters a-z
+        // this has 32 bits set to 0
         int checker = 0;
         for (int i = 0; i < str.length(); i++){
             int val = str.charAt(i) - 'a';
-            if ((checker & (1 << val)) > 0) {
+            System.out.println(val);
+            // if that bit is already set in checker,
+            // return false
+            if ((checker & (1 << val)) > 0) { //<< - shift bit pattern to the left
                 return false;
             }
+            // otherwise update and continue by
+            // setting that bit in the checker
             checker |= (1 << val);
         }
+        // no duplicates encountered, return true
         return true;
     }
 }
