832fa3fcce242f26fc801c3b1d9a42d73417d205
JayJay <jwanjohi95@gmail.com>
2018-11-22 22:00:37 +0000

    Test permutation code

--- src/ArraysAndStrings/CheckPermutation.java
+++ src/ArraysAndStrings/CheckPermutation.java
@@ -1,12 +1,17 @@
 package ArraysAndStrings;
 
 public class CheckPermutation {
-    String sort (String s) {
+    public static void main(String[] args) {
+        String firstString = "Mother";
+        String secondString = "mother";
+        System.out.println("Permutation test: " + permutation(firstString, secondString));
+    }
+    public static String sort (String s) {
         char[] content = s.toCharArray();
         java.util.Arrays.sort(content);
         return new String(content);
     }
-    boolean permutation(String s, String t){
+    public static boolean permutation(String s, String t){
         if(s.length() != t.length()){
             return false;
         }
